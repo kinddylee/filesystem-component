2a1f4e48718915258023725cd04bc23ae3835832
nilopc <nilopc@gmail.com>
2013-08-20 21:15:42 +0200

    Added LICENSE

--- src/NilPortugues/Component/FileSystem/FileSystem.php
+++ src/NilPortugues/Component/FileSystem/FileSystem.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * Author: Nil Portugués Calderó <contact@nilportugues.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace NilPortugues\Component\FileSystem;
 
 use \NilPortugues\Component\FileSystem\Exceptions\FileSystemException as FileSystemException;
